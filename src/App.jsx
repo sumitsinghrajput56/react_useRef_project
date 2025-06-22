3a7d5e0aea35f3cc01f3b68daf1db0345dfbbb78
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-06-22 19:07:42 +0530

    when we create with useRef working increasing variable value because it keeps value in object form in current when re-render in state changes dont effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count,setCount]=useState(0);
 
-  var val=1;
+  var val=useRef(0);
 
   useEffect(()=>{
     console.log("mai render ho gya");
@@ -12,7 +12,7 @@ function App() {
 
   function HandleIncrementClick()
   {
-    val=val+1;
+    val.current=val.current+1;
 
     console.log(val);
     
