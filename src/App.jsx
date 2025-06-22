4650715e6b7b1b82ffa64403dbf34ef6729445cc
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-06-22 19:05:17 +0530

    unable to change variable value

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  var val=1;
+
+  useEffect(()=>{
+    console.log("mai render ho gya");
+  });
+
+  function HandleIncrementClick()
+  {
+    val=val+1;
+
+    console.log(val);
+    
+    setCount(count+1);
+  }
   return (
     <>
-     
+      <button onClick={HandleIncrementClick}>Increment</button>
+      <p>count:{count}</p>
+
     </>
   )
 }
